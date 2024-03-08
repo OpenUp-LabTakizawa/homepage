c977ed6fd04e77cf89c617af767d71c942daf967
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-08 16:00:42 +0900

    ✨feat: update navbar with daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -6,14 +7,8 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
 export default config
