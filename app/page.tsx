a8078603288a4dc05fb1f66c9107a892cbfe6c9c
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-13 13:14:23 +0900

    ✨feat: add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/iwate_mountain.webp"
-        width={500}
-        height={500}
-        alt="Iwate Mountain"
-        className="hero min-h-screen"
-      />
-      <div>
+      <div className="hero min-h-screen bg-[url('/iwate_mountain.webp')]">
         <div className="hero-overlay bg-opacity-60" />
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
@@ -28,12 +21,13 @@ export default function Home() {
             <p className="mb-5">
               岩手県立大学と滝沢市が産官学連携を目的として設立した滝沢市IPUイノベーションセンターにOPEN
               UPラボ滝沢は入居しています。
-              <br />
+            </p>
+            <p className="mb-5">
               ラボを取り巻くコミュニティとの協働や新たな取り組みで連携し、地域社会への貢献を行っています。
             </p>
-            <button type="button" className="btn btn-warning">
+            <Link href="/about" className="btn btn-warning">
               詳しくはこちら
-            </button>
+            </Link>
           </div>
         </div>
       </div>
