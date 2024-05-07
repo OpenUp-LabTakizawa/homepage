8ab662d8d7024d34fd5f0021cf03be0d4f84e107
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-05-07 18:10:34 +0900

    ✨feat: add github mark to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,21 @@
-export function Footer() {
+import Image from 'next/image'
+import Link from 'next/link'
+import type React from 'react'
+
+export function Footer(): React.JSX.Element {
   return (
-    <footer className="footer footer-center bg-base-300 p-4 text-base-content">
+    <footer className="footer flex flex-row items-center bg-base-300 justify-center p-4 text-base-content">
       <aside>
         <p>Copyright © Open Up Group Inc. All rights reserved.</p>
       </aside>
+      <nav>
+        <Link
+          href="https://github.com/OpenUp-LabTakizawa/homepage"
+          target="_blank"
+        >
+          <Image src="/github-mark.svg" alt="GitHub" width={24} height={24} />
+        </Link>
+      </nav>
     </footer>
   )
 }
