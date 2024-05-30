3382cbd358e850ad2a199494b75279955804d7de
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-05-30 15:21:22 +0900

    ✨feat: add heading

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,17 +1,10 @@
 import type React from 'react'
-import { Breadcrumb } from '../components/breadcrumb'
-import { navigation } from '../components/navbar'
-
-const info = navigation.filter((item) => item.href === '/info')[0]
+import { Heading } from '../components/heading'
 
 export default function Info(): React.JSX.Element {
   return (
     <main>
-      <h1 className="text-5xl font-bold flex justify-center">
-        <info.icon className={`size-12 mr-1 ${info.color}`} />
-        {info.name}
-      </h1>
-      <Breadcrumb crumb={info} />
+      <Heading href="/info" />
     </main>
   )
 }
