a8893cd21cee775ace5ee697656b84d1b79f36f3
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-04-02 14:40:27 +0900

    🆙bump: version up dependencies (#693)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
+    isrFlushToDisk: false,
     ppr: true,
     reactCompiler: true,
     viewTransition: true,
